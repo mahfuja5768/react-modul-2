13dfb685ac356387845ce97001032dbc246b286b
Mahfuja Ahsan Muna <121941219+mahfuja5768@users.noreply.github.com>
2024-01-14 01:33:17 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Gallery from "./components/Gallery";
+
+function App() {
+  return (
+    <div>
+      <Gallery />
+      <hr />
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Gallery.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { sculptureList } from "./data";
+
+export default function Gallery() {
+  //   let index = 0;
+
+  //   function handleClick() {
+  //     index += 1;
+  //   }
+
+  let [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleClick() {
+    setIndex(index + 1);
+  }
+
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handleClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
+  );
+}
+
